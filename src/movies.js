a3cab42b85282b7d959d32875392977253181751
Hernando Bifaretti <hbifaretti@gmail.com>
2022-06-02 23:43:21 +0200

    update-iteration-6-done

--- src/movies.js
+++ src/movies.js
@@ -31,7 +31,7 @@ function howManyMovies(movies) {
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(movies) {
   const totalAvgOfMovies = movies.reduce((acc, eachElement) => {
-    if (eachElement) return acc + eachElement.score;
+    if (eachElement.score) return acc + eachElement.score;
     else {
       acc;
     }
@@ -45,20 +45,52 @@ function scoresAverage(movies) {
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(movies) {
-  const avgScoreDrama = movies.reduce((acc, eachElement) => {
-    let count = 0;
-    if (eachElement.genre.includes('Drama')) {
-      count++;
-    }
-    return acc;
+  const dramaMovies = movies.filter((drama) => {
+    return drama.genre.includes('Drama');
+  });
+  const AvgMovie = movies.reduce((acc, eachElement) => {
+    return acc + eachElement.score;
   }, 0);
+  if (dramaMovies === 0) return 0;
+  let total = AvgMovie / dramaMovies.length;
+  return Number(total.toFixed(2));
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(movies) {
+  let newArrayMovies = JSON.parse(JSON.stringify(movies));
+  const growingOrderMovies = newArrayMovies.sort((first, second) => {
+    if (first.year < second.year) {
+      return -1;
+    } else if (first.year > second.year) {
+      return 1;
+    } else {
+      if (first.year === second.year) {
+        if (first.title < second.title) {
+          return -1;
+        }
+      }
+    }
+  });
+  return growingOrderMovies;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically(movies) {
+  let newArrayMovies = JSON.parse(JSON.stringify(movies));
+  const alphabeticOrder = newArrayMovies.sort((first, second) => {
+    if (first.title < second.title) {
+      return -1;
+    } else if (first.title > second.title) {
+      return 1;
+    }
+  });
+  const onlyTitles = alphabeticOrder.map((element) => {
+    return element.title;
+  });
+
+  return onlyTitles.slice(0, 20);
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
